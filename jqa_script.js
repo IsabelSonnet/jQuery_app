4b99f62457fb52add3a004afe2a740299dcbf7d8
Samantha <samantha@Samanthas-MacBook-Pro.local>
2020-03-17 20:22:27 -0700

    All boxes work and move with double click

--- jqa_script.js
+++ jqa_script.js
@@ -8,9 +8,49 @@ $(document).ready(() => {
     });
 
     $box1.on({
-        click: () => {
+        mouseleave: () => {
             $pinkimg.hide();
         }
+    });
+
+    $pinkimg.dblclick(() => {
+        $box1.css("margin-left", "+=100");
+    })
+
+    var $lightblueimg = $("#lightblueimg");
+    var $box2 = $("#box2");
+    $lightblueimg.hide();
+
+    $box2.mouseover(() => {
+        $lightblueimg.show();
+    });
+
+    $box2.on({
+        mouseleave: () => {
+            $lightblueimg.hide();
+        }
+    });
+
+    $lightblueimg.dblclick(() => {
+        $box2.css("margin-left", "+=100");
+    });
+
+    var $limegreenimg = $("#limegreenimg");
+    var $box3 = $("#box3");
+    $limegreenimg.hide();
+
+    $box3.mouseover(() => {
+        $limegreenimg.show();
+    });
+
+    $box3.on({
+        mouseleave: () => {
+            $limegreenimg.hide();
+        }
+    });
+
+    $limegreenimg.dblclick(() => {
+        $box3.css("margin-left", "+=100");
     });
 
     var $tealimg = $("#tealimg");
@@ -22,8 +62,13 @@ $(document).ready(() => {
     });
 
     $box4.on({
-        click: () => {
+        mouseleave: () => {
             $tealimg.hide();
         }
     });
+
+    $tealimg.dblclick(() => {
+        $box4.css("margin-left", "+=100");
+    });
+
 });
